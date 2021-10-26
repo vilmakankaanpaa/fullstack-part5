fcedea5d3671e2eee7e3c0937456edea4f873adb
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2021-10-26 10:20:11 +0200

    Exercise 5.2.

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+// import loginRouter from '../../../Part4/bloglist/controllers/login'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
@@ -9,10 +10,20 @@ const App = () => {
   const [password, setPassword] = useState('') 
   const [user, setUser] = useState(null)
 
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
   }, [])
 
   const handleLogin = async (event) => {
@@ -22,7 +33,10 @@ const App = () => {
         username, password,
       })
       
-      //blogService.setToken(user.token)
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -32,7 +46,15 @@ const App = () => {
       //   setErrorMessage(null)
       // }, 5000)
     }
-  }    
+  }  
+  
+  const handleLogout = () => {
+    window.localStorage.removeItem(
+      'loggedNoteappUser'
+    )
+    blogService.setToken('')
+    setUser(null)
+  }
 
   const loginForm = () => (
     <form onSubmit={handleLogin}>
@@ -64,7 +86,7 @@ const App = () => {
       {user === null ?
         loginForm() :
         <div>
-          <p>{user.name} logged-in</p>
+          <p>{user.name} logged-in <button onClick={handleLogout}>logout</button></p>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
           )}  
