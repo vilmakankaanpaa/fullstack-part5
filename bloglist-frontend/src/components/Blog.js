2910ec201db669eb3df03efa9fd7d8f7d4a7bf22
Vilma Kankaanpää <vilma.kankaanpaa@aalto.fi>
2021-11-08 16:14:49 +0200

    Exercise 5.7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,36 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+const Blog = ({blog}) => {
+
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+          {blog.title} <button onClick={toggleVisibility}>view</button> 
+      </div>
+      <div style={showWhenVisible}>
+        {blog.title} <button onClick={toggleVisibility}>hide</button> 
+        <br/> {blog.url}
+        <br/> likes {blog.likes} <button>like</button>
+        <br/> {blog.author} 
+      </div> 
+    </div> 
+  )
+}
 
 export default Blog
